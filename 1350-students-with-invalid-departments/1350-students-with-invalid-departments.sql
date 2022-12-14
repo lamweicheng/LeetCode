da197fed2d5caa4763a2956510706846358a8ffc
Anderson Lam Wei Cheng <lamweicheng@yahoo.com.my>
2022-12-14 01:14:29 -0800

    Time: 1661 ms (8.97%), Space: 0B (100.00%) - LeetHub

--- 1350-students-with-invalid-departments/1350-students-with-invalid-departments.sql
+++ 1350-students-with-invalid-departments/1350-students-with-invalid-departments.sql
@@ -1,6 +1,4 @@
 # Write your MySQL query statement below
-SELECT s.id, s.name
-FROM Departments d
-RIGHT JOIN Students s
+SELECT s.id, s.name FROM Departments d RIGHT JOIN Students s
 ON d.id = s.department_id
 WHERE d.id IS NULL
