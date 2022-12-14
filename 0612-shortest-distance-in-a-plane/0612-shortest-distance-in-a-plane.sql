f549222f99d74f895035a410b15f7827d842e488
Anderson Lam Wei Cheng <lamweicheng@yahoo.com.my>
2022-12-13 23:44:16 -0800

    Time: 271 ms (76.19%), Space: 0B (100.00%) - LeetHub

---
+++ 0612-shortest-distance-in-a-plane/0612-shortest-distance-in-a-plane.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+SELECT
+    ROUND(SQRT(MIN((POW(p1.x - p2.x, 2) + POW(p1.y - p2.y, 2)))),2) AS shortest
+FROM
+    point2d AS p1
+        JOIN
+    point2d AS p2 ON (p1.x <= p2.x AND p1.y < p2.y)
+        OR (p1.x <= p2.x AND p1.y > p2.y)
+        OR (p1.x < p2.x AND p1.y = p2.y)
+;
