27c4b8d2da8f90e92ed48e55baef41497f9812c4
Anderson Lam Wei Cheng <lamweicheng@yahoo.com.my>
2022-12-15 00:11:35 -0800

    Time: 1056 ms (57.58%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,3 +1,3 @@
 # Please write a DELETE statement and DO NOT write a SELECT statement.
 # Write your MySQL query statement below
-DELETE p1 FROM Person AS p1, Person AS p2 WHERE p1.Email = p2.Email AND p1.Id > p2.Id
+DELETE p1 FROM Person AS p1, Person AS p2 WHERE p1.email = p2.email AND p1.Id > p2.Id
