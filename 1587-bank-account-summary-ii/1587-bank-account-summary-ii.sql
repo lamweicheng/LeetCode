3d14b438ba003a5e6d49de56bfcbe9da02d5347a
Anderson Lam Wei Cheng <lamweicheng@yahoo.com.my>
2022-12-14 18:12:51 -0800

    Time: 833 ms (50.83%), Space: 0B (100.00%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-SELECT u.name,  sum(t.amount) AS balance FROM Users AS U JOIN Transactions AS T ON U.account = T.account  GROUP BY T.account HAVING sum(t.amount) > 10000
+SELECT u.name,  sum(t.amount) AS balance FROM Users AS U JOIN Transactions AS T ON U.account = T.account  GROUP BY T.account HAVING balance > 10000
