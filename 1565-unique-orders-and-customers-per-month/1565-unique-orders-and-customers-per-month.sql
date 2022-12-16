e5c2c77ccaa27b3a6ca8a9a441a404965e36114b
Anderson Lam Wei Cheng <lamweicheng@yahoo.com.my>
2022-12-16 12:49:54 -0800

    Time: 1088 ms (21.76%), Space: 0B (100.00%) - LeetHub

--- 1565-unique-orders-and-customers-per-month/1565-unique-orders-and-customers-per-month.sql
+++ 1565-unique-orders-and-customers-per-month/1565-unique-orders-and-customers-per-month.sql
@@ -1,8 +1,10 @@
 # Write your MySQL query statement below
 select
-    date_format(order_date, '%Y-%m') month,
-    count(*) order_count,
+   date_format(order_date, '%Y-%m') month, count(order_id) order_count,
     count(distinct customer_id) customer_count
-from orders
-where invoice > 20
-group by date_format(order_date, '%Y-%m')
+from
+     Orders 
+where 
+     invoice>20 
+group by
+      month;
