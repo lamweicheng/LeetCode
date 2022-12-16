499055fca17f93119a3cb44a1843f47657d421ee
Anderson Lam Wei Cheng <lamweicheng@yahoo.com.my>
2022-12-16 12:49:11 -0800

    Time: 871 ms (24.50%), Space: 0B (100.00%) - LeetHub

--- 2026-low-quality-problems/2026-low-quality-problems.sql
+++ 2026-low-quality-problems/2026-low-quality-problems.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT problem_id 
-FROM Problems 
-WHERE (likes)/(likes + dislikes) <= 0.6
+SELECT problem_id
+FROM problems
+WHERE (likes * 100 / (likes + dislikes)) < 60
 ORDER BY problem_id ASC;
