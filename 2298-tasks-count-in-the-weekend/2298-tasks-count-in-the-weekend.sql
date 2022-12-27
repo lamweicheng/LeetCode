26975f6f7e051cebb61b4e82f0e7df17126552f5
Anderson Lam Wei Cheng <lamweicheng@yahoo.com.my>
2022-12-27 15:57:26 -0800

    Time: 379 ms (93.40%), Space: 0B (100.00%) - LeetHub

--- 2298-tasks-count-in-the-weekend/2298-tasks-count-in-the-weekend.sql
+++ 2298-tasks-count-in-the-weekend/2298-tasks-count-in-the-weekend.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-select SUM(WEEKDAY(submit_date) >= 5) AS weekend_cnt, SUM(WEEKDAY(submit_date) < 5) AS working_cnt FROM tasks;
+select SUM(WEEKDAY(submit_date) >= 5) AS weekend_cnt, 
+SUM(WEEKDAY(submit_date) < 5) AS working_cnt 
+FROM tasks;
